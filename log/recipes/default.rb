cacb9955937133e1a69a73901e26e59c051f2406
borntorock <nitinagarwal3006@gmail.com>
2016-04-05 09:52:07 +0000

    updated default recipe

--- log/recipes/default.rb
+++ log/recipes/default.rb
@@ -7,9 +7,9 @@ node[:deploy].each do |application, deploy|
     cd shared
     mkdir order-logs mysql_logs redis_logs
     chmod -R 777 order-logs mysql_logs redis_logs
-    ln -s #{deploy[:deploy_to]}/shared #{deploy[:deploy_to]}/current/order-logs
-    ln -s #{deploy[:deploy_to]}/shared #{deploy[:deploy_to]}/current/mysql_logs
-    ln -s #{deploy[:deploy_to]}/shared #{deploy[:deploy_to]}/current/redis_logs
+    ln -s #{deploy[:deploy_to]}/shared/order-logs #{deploy[:deploy_to]}/current/order-logs
+    ln -s #{deploy[:deploy_to]}/shared/mysql_logs #{deploy[:deploy_to]}/current/mysql_logs
+    ln -s #{deploy[:deploy_to]}/shared/redis_logs #{deploy[:deploy_to]}/current/redis_logs
     chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/order-logs
     chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/mysql_logs
     chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/redis_logs
