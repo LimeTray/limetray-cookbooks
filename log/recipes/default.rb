05b2913c9be27f07b389258118415c0a793bc886
himanshujain <himanshujain.2792@gmail.com>
2016-04-12 00:29:34 +0530

    directory check in recipes

--- log/recipes/default.rb
+++ log/recipes/default.rb
@@ -1,9 +1,9 @@
 node[:deploy].each do |application, deploy|
   script "default" do
-    if deploy[:application_type] != 'php'
-        Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
-        next
-    end
+    # if deploy[:application_type] != 'php'
+    #     Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    #     next
+    # end
     if application == 'limetraycms'
         interpreter "bash"
         user "root"
