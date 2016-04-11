bcc28b0c763e5ae00907d5b38bce6e9fbe02be35
himanshujain <himanshujain.2792@gmail.com>
2016-04-11 17:56:14 +0530

    log directory creation changes

--- log/recipes/default.rb
+++ log/recipes/default.rb
@@ -5,12 +5,12 @@ node[:deploy].each do |application, deploy|
     cwd "#{deploy[:deploy_to]}"
     code <<-EOH
     cd shared
-    mkdir -p order-logs mysql_logs redis_logs "logs/error"
+    mkdir -p order-logs mysql_logs redis_logs logs/error
     chmod -R 777 order-logs mysql_logs redis_logs logs
     ln -s #{deploy[:deploy_to]}/shared/order-logs #{deploy[:deploy_to]}/current/order-logs
     ln -s #{deploy[:deploy_to]}/shared/mysql_logs #{deploy[:deploy_to]}/current/mysql_logs
     ln -s #{deploy[:deploy_to]}/shared/redis_logs #{deploy[:deploy_to]}/current/redis_logs
-    ln -s #{deploy[:deploy_to]}/shared/logs/error #{deploy[:deploy_to]}/current/logs/error
+    ln -s #{deploy[:deploy_to]}/shared/logs #{deploy[:deploy_to]}/current/logs
     chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/order-logs
     chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/mysql_logs
     chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/redis_logs
