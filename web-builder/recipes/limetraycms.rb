93da8cefdede349dd5dcdc9cb02cfd6df5524b16
himanshujain <himanshujain.2792@gmail.com>
2016-04-12 12:53:18 +0530

    indentatio fixes

--- web-builder/recipes/limetraycms.rb
+++ web-builder/recipes/limetraycms.rb
@@ -1,15 +1,15 @@
 node[:deploy].each do |application, deploy|
   script "limetraycms" do
-  	if application == 'limetraycms'
-	    interpreter "bash"
-	    user "root"
-	    cwd "#{deploy[:deploy_to]}"
-	    code <<-EOH
-	    if [ -d "/mnt/srv/www/web-builder/current" ]; then
-	    	cp /mnt/srv/www/web-builder/current #{deploy[:deploy_to]}
-	    fi 
-	    EOH
-	end
+    if application == 'limetraycms'
+        interpreter "bash"
+        user "root"
+        cwd "#{deploy[:deploy_to]}"
+        code <<-EOH
+        if [ -d "/mnt/srv/www/web-builder/current" ]; then
+            cp /mnt/srv/www/web-builder/current #{deploy[:deploy_to]}
+        fi 
+        EOH
+    end
   end
 end
 
--- web-builder/recipes/web-builder.rb
+++ web-builder/recipes/web-builder.rb
@@ -1,15 +1,15 @@
 node[:deploy].each do |application, deploy|
   script "web-builder" do
-  	if application == 'web-builder'
-	    interpreter "bash"
-	    user "root"
-	    cwd "#{deploy[:deploy_to]}"
-	    code <<-EOH
-	    if [ -d "/mnt/srv/www/limetraycms/current" ]; then
-	    	cp #{deploy[:deploy_to]} /mnt/srv/www/limetraycms/current
-	    fi
-	    EOH
-	end
+    if application == 'web-builder'
+        interpreter "bash"
+        user "root"
+        cwd "#{deploy[:deploy_to]}"
+        code <<-EOH
+        if [ -d "/mnt/srv/www/limetraycms/current" ]; then
+            cp #{deploy[:deploy_to]} /mnt/srv/www/limetraycms/current
+        fi
+        EOH
+    end
   end
 end
 
