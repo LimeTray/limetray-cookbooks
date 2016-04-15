a2f0d2d968f5602ba7a121f483c6809cf46d76fe
Puneet Sharma <puneet@unicommerce.com>
2016-04-15 15:38:55 +0530

    Updated script

--- web_builder/recipes/web_builder.rb
+++ web_builder/recipes/web_builder.rb
@@ -1,11 +1,2 @@
 node[:deploy].each do |application, deploy|
-  script "web_builder" do
-    if application == 'web_builder'
-        interpreter "bash"
-        user "root"
-        cwd "#{deploy[:deploy_to]}"
-    end
-  end
 end
-
-
