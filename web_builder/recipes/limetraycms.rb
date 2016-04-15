436b3328a2093443fc1bcfe4eee8c9beaac5902d
Puneet Sharma <puneet@unicommerce.com>
2016-04-16 00:11:55 +0530

    Update script to copy web_builder

--- web_builder/recipes/limetraycms.rb
+++ web_builder/recipes/limetraycms.rb
@@ -1,2 +1,8 @@
 node[:deploy].each do |application, deploy|
+bash 'copy_web_builder' do
+code <<-EOH
+mkdir -p /mnt/srv/www/limetraycms/current/web_builder/
+cp -r /mnt/srv/www/web_builder/current/* /mnt/srv/www/limetraycms/current/web_builder/
+EOH
+end
 end
