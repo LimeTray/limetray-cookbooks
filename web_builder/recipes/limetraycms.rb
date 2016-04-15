47e00fd69f9df414dfe375ff9acc358ab2519485
Puneet Sharma <puneet@unicommerce.com>
2016-04-15 23:06:33 +0530

    Removed all entries from log

--- web_builder/recipes/limetraycms.rb
+++ web_builder/recipes/limetraycms.rb
@@ -1,9 +1,2 @@
 node[:deploy].each do |application, deploy|
-  script "limetraycms" do
-    if application == 'limetraycms'
-        interpreter "bash"
-        user "root"
-        cwd "#{deploy[:deploy_to]}"
-    end
-  end
 end
