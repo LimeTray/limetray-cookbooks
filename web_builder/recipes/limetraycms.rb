afa124e064f67b7f79fca9b08e2a6b36730da5b0
Puneet Sharma <puneet@unicommerce.com>
2016-04-15 13:39:54 +0530

    Added -r in cp

--- web_builder/recipes/limetraycms.rb
+++ web_builder/recipes/limetraycms.rb
@@ -5,6 +5,7 @@ node[:deploy].each do |application, deploy|
         user "root"
         cwd "#{deploy[:deploy_to]}"
         code <<-EOH
+	echo #{deploy[:deploy_to]}
         if [ -d "/mnt/srv/www/web_builder/current" ]; then
             cp -r /mnt/srv/www/web_builder/current #{deploy[:deploy_to]}
         fi 
