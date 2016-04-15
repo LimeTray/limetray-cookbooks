369e73630cab8e776ad2b3002c569aaf9ce09364
Puneet Sharma <puneet@unicommerce.com>
2016-04-15 15:19:42 +0530

    Updated script

--- web_builder/recipes/limetraycms.rb
+++ web_builder/recipes/limetraycms.rb
@@ -4,12 +4,6 @@ node[:deploy].each do |application, deploy|
         interpreter "bash"
         user "root"
         cwd "#{deploy[:deploy_to]}"
-        code <<-EOH
-	echo #{deploy[:deploy_to]}
-        if [ -d "/mnt/srv/www/web_builder/current" ]; then
-            cp -r /mnt/srv/www/web_builder/current #{deploy[:deploy_to]}
-        fi 
-        EOH
     end
   end
 end
--- web_builder/recipes/web_builder.rb
+++ web_builder/recipes/web_builder.rb
@@ -1,17 +1,9 @@
 node[:deploy].each do |application, deploy|
   script "web_builder" do
     if application == 'web_builder'
-	print node["deploy"]["appshortname"]["symlinks"]
-	echo node["deploy"]["appshortname"]["symlinks"]
         interpreter "bash"
         user "root"
         cwd "#{deploy[:deploy_to]}"
-        code <<-EOH
-	mkdir -p /mnt/srv/www/limetraycms/current/web_builder/
-        if [ -d "/mnt/srv/www/limetraycms/current" ]; then
-            cp -r /mnt/srv/www/web_builder/current /mnt/srv/www/limetraycms/current/web_builder/
-        fi
-        EOH
     end
   end
 end
