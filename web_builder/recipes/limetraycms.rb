62f6f43074f25cf372813fb415ac10612fbec9d6
Puneet Sharma <puneet@unicommerce.com>
2016-04-16 22:48:41 +0530

    Update script for permission issue

--- web_builder/recipes/limetraycms.rb
+++ web_builder/recipes/limetraycms.rb
@@ -3,6 +3,7 @@ bash 'copy_web_builder' do
 code <<-EOH
 mkdir -p /mnt/srv/www/limetraycms/current/web_builder/
 cp -r /mnt/srv/www/web_builder/current/* /mnt/srv/www/limetraycms/current/web_builder/
+chown -R #{deploy[:user]}:#{deploy[:group]} /mnt/srv/www/limetraycms/current/web_builder/
 EOH
 end
 end
