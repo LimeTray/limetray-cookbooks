3fa3f99d45c879aa6888b1f2dd309369aa3738fc
himanshujain <himanshujain.2792@gmail.com>
2016-04-21 01:02:58 +0530

    recipe  changes for if condition

--- crm/recipes/storage_permissions.rb
+++ crm/recipes/storage_permissions.rb
@@ -1,12 +1,12 @@
 node[:deploy].each do |application, deploy|
   script "storage_permissions" do
-  	Chef::Log.info(application)
-	interpreter "bash"
-	user "root"
-	cwd "#{deploy[:deploy_to]}/current"
-	code <<-EOH
-	chmod -R 777 app/storage/ 
-	EOH
+  	Chef::Log.info(node[:deploy][:application_name])
+	# interpreter "bash"
+	# user "root"
+	# cwd "#{deploy[:deploy_to]}/current"
+	# code <<-EOH
+	# chmod -R 777 app/storage/ 
+	# EOH
 
   end
 end
