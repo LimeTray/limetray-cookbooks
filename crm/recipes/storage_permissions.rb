6f8d60f3ff074dd735ad0e6dd5b57233d8d226ec
himanshujain <himanshujain.2792@gmail.com>
2016-04-21 01:26:22 +0530

    condition for crm directory

--- crm/recipes/storage_permissions.rb
+++ crm/recipes/storage_permissions.rb
@@ -1,13 +1,14 @@
 node[:deploy].each do |application, deploy|
   Chef::Log.info(node[:deploy][:application_name])
   script "storage_permissions" do
-  	Chef::Log.info(node[:deploy][:application_name])
-	# interpreter "bash"
-	# user "root"
-	# cwd "#{deploy[:deploy_to]}/current"
-	# code <<-EOH
-	# chmod -R 777 app/storage/ 
-	# EOH
+	interpreter "bash"
+	user "root"
+	cwd "#{deploy[:deploy_to]}/current"
+	code <<-EOH
+	if [ -d "app/storage/" ]; then
+		chmod -R 777 app/storage/ 
+	fi
+	EOH
 
   end
 end
--- crm/recipes/symlink_storage.rb
+++ crm/recipes/symlink_storage.rb
@@ -1,16 +1,17 @@
 node[:deploy].each do |application, deploy|
   script "symlink_storage" do
- #    interpreter "bash"
- #    user "root"
- #    cwd "#{deploy[:deploy_to]}"
- #    code <<-EOH
-   
-	# chmod -R 777 current/app/storage
- #    mv current/app/storage/* shared
- #    rm -rf current/storage
- #    ln -s #{deploy[:deploy_to]}/shared #{deploy[:deploy_to]}/current/app/storage
- #    chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/app/storage
- #    EOH
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}"
+    code <<-EOH
+    if [ -d "current/app/storage" ]; then
+    	chmod -R 777 current/app/storage
+        mv current/app/storage/* shared
+        rm -rf current/storage
+        ln -s #{deploy[:deploy_to]}/shared #{deploy[:deploy_to]}/current/app/storage
+        chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/app/storage
+    fi
+    EOH
   end
 end
 
