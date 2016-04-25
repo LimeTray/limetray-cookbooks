a7c1b32618418931344fe8c81e037668c2bbf5e6
himanshujain <himanshujain.2792@gmail.com>
2016-04-25 16:59:25 +0530

    changes in log recipe

--- log/recipes/default.rb
+++ log/recipes/default.rb
@@ -11,7 +11,7 @@ node[:deploy].each do |application, deploy|
 	    ln -s #{deploy[:deploy_to]}/shared/order-logs #{deploy[:deploy_to]}/current/order-logs
 	    ln -s #{deploy[:deploy_to]}/shared/mysql_logs #{deploy[:deploy_to]}/current/mysql_logs
 	    ln -s #{deploy[:deploy_to]}/shared/redis_logs #{deploy[:deploy_to]}/current/redis_logs
-	    ln -s #{deploy[:deploy_to]}/shared/logs #{deploy[:deploy_to]}/current/logs/
+	    ln -s #{deploy[:deploy_to]}/shared/logs #{deploy[:deploy_to]}/current/logs
 	    chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/order-logs
 	    chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/mysql_logs
 	    chown -h #{deploy[:user]}:#{deploy[:group]} #{deploy[:deploy_to]}/current/redis_logs
