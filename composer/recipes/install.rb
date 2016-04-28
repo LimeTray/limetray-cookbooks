53d512ab1d4644c249c1947f0083234fade5865d
himanshujain <himanshujain.2792@gmail.com>
2016-04-29 01:03:15 +0530

    fixes

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -5,7 +5,7 @@ node[:deploy].each do |application, deploy|
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
-    echo -e "{\"github-oauth\":{\""github.com"\": \"5a6c445f1f2e7be54c4b90f8fb37e9f7261ce1a5\" }}" > /root/.composer/auth.json
+ 
     php composer.phar install --no-dev --no-interaction --prefer-dist
     EOH
     only_if { ::File.exist?("#{deploy[:deploy_to]}/current/composer.json")}
